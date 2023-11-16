0be8f8dc1cea33102f5b3e309cd5285dc5160898
EUFrosty <bojanvelickovic98@gmail.com>
2023-11-16 22:14:35 +0100

    pakao.c

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,27 @@
 #include "mainwindow.hpp"
 #include "./ui_mainwindow.h"
 
+#include <QSplitter>
+#include <QFileSystemModel>
+
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QFileSystemModel *model = new QFileSystemModel;
+    model->setRootPath(QDir::currentPath());
+    ui->treeView ->setModel(model);
+
+    model->setRootPath(QDir::rootPath());
+    model->setFilter(QDir::AllEntries | QDir::NoDotAndDotDot);
+    ui->tableView ->setModel(model);
+    QObject::connect(ui->tableView, &QTableView::doubleClicked, ui->tableView, &QTableView::setRootIndex);
+    ui->tableView->show();
+
+
 }
 
 MainWindow::~MainWindow()
