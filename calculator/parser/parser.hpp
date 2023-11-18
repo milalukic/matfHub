a2d93851a11e85738d8a5db4a4a692b079d2b6d9
DzzzD <mi20098@alas.matf.bg.ac.rs>
2023-11-18 15:21:16 +0100

    add parser functionality

--- calculator/parser/parser.hpp
+++ calculator/parser/parser.hpp
@@ -4,20 +4,30 @@
 #include <vector>
 class Parser{
 
+#define NUMVARS 26
+
 public:
 
     Parser();
     ~Parser();
     double eval_exp(char *exp);
     char errormsg[64];
+    
 
-private:
+protected:
 
-    const int NUMVARS = 26;
+    void eval_exp1(double &result);
+    void eval_exp2(double &result);
+    void eval_exp3(double &result);
+    void eval_exp4(double &result);
+    void eval_exp5(double &result);
+    void eval_exp6(double &result);
+    void get_token();
+
     char *exp_ptr; // points to the expression
     char token[256]; // holds current token
     char tok_type; // holds token's type
-    std::vector<double>(NUMVARS); // holds variable's values
+    double vars[NUMVARS]; // holds variable's values
 };
 
 
