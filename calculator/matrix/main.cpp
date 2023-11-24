b02b77a4998e29dfbe66f224f5e28c2c6d3e76d8
DzzzD <mi20098@alas.matf.bg.ac.rs>
2023-11-24 14:15:46 +0100

    prevedeno na srpski, funkcije dinamicki prave matrice

--- calculator/matrix/main.cpp
+++ calculator/matrix/main.cpp
@@ -5,11 +5,10 @@ int main(){
 
     // Matrix m1 = Matrix(3, 3, "dd");
     // Matrix m2 = Matrix(3, 3, "dd1");
-    Matrix *m1 = new Matrix(4, 3, "ddd");
+    Matrix *m1 = new Matrix(3, 3, "ddd");
     Matrix *m2 = new Matrix(3, 3, "dddd");
     
     arma::mat kopija = m1->data();
-    
     kopija(0, 0) = 5;
     kopija(1, 1) = 6;
     m1->data(kopija);
@@ -17,14 +16,16 @@ int main(){
     kopija(0, 1) = 6;
     m2->data(kopija);
 
-    std::cout << *m1 << std::endl;
-    m1->transpose();
-    std::cout << *m1 << std::endl;
+    Matrix *m3 = ++m2;
+    std::cout << *m3 << std::endl;
+    std::cout << *m3 << std::endl;
 
-    Matrix jedinicna = Matrix(3, 3).diag();
-    std::cout << jedinicna << std::endl;
 
+    std::cout << "\tDeleting block start" << std::endl;
     delete m1; m1 = nullptr; 
+    delete m3; m3 = nullptr;
+    std::cout << "\tDeleting block end" << std::endl;
+    
     std::cout << "EOF" << std::endl;
     return 0;
 }
---
+++ calculator/matrix/matrix.hpp
@@ -0,0 +1,58 @@
+#ifndef MATRIX_HPP
+
+#define MATRIX_HPP
+
+#include <iostream>
+#include <vector>
+#include <armadillo>
+
+class Matrix{
+
+public:
+//konstruktori
+    Matrix(unsigned dimension1 = 1, unsigned dimension2 = 1, std::string matrixName = "matrix name");
+    //destruktor                                      
+    ~Matrix();
+
+    //get
+    unsigned dimension1() const;
+    unsigned dimension2() const;
+    std::string matrixName() const;
+    arma::mat data() const;
+
+    //set
+    void dimension1(unsigned u);
+    void dimension2(unsigned u);
+    void matrixName(std::string name);
+    void data(arma::mat Matrix);
+
+    //functions
+    void transpose();
+    void inverse();
+    Matrix *ones();
+    Matrix *diag();
+
+    //operators
+    Matrix *operator + (const Matrix &other) const;
+    Matrix *operator - (const Matrix &other) const;
+    Matrix operator *(const Matrix &other) const;
+    Matrix operator /(const Matrix &other) const;
+    Matrix &operator ++();
+    Matrix *operator ++(int);  
+    Matrix *operator-() const;
+
+    bool operator == (const Matrix &other) const;
+    bool operator != (const Matrix &other) const;
+
+    
+private:
+    unsigned _dimension1;
+    unsigned _dimension2;
+    std::string _matrixName;
+    arma::mat *_data;
+};
+
+std::ostream &operator<<(std::ostream &out, const Matrix &value);
+std::istream &operator>>(std::istream &in, Matrix &value);
+
+#endif //MATRIX_HPP
