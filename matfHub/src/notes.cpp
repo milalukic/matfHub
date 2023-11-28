2d7819e58b48bf87b586014c5f61d6f0bb4b3c28
Mila Lukic <milalukic222@gmail.com>
2023-11-28 22:48:43 +0100

    Promenjena funkcija za cuvanje tako da radi Save As ako fajl nije pre cuvan, a Save ako je vec cuvan (nadam se da ova recenica ima smisla).

--- matfHub/src/notes.cpp
+++ matfHub/src/notes.cpp
@@ -30,20 +30,34 @@ void Notes::open_clicked(Ui::MainWindow *ui, QWidget *parent){
 }
 
 void Notes::save_clicked(Ui::MainWindow *ui, QWidget *parent){
-    QString fileName = QFileDialog::getSaveFileName(parent, "Sacuvaj.");
-    QFile file(fileName);
 
-    if(!file.open(QFile::WriteOnly | QFile::Text)){
-        return;
+    if (currentFile.isEmpty()) {
+        QString fileName = QFileDialog::getSaveFileName(parent, "Sacuvaj.");
+        QFile file(fileName);
+
+        if (!file.open(QFile::WriteOnly | QFile::Text)) {
+            return;
+        }
+
+        currentFile = fileName;
+        parent->setWindowTitle(fileName);
+
+        QTextStream out(&file);
+        QString text = ui->textEdit->toPlainText();
+        out << text;
+        file.close();
+    } else {
+        QFile file(currentFile);
+
+        if (!file.open(QFile::WriteOnly | QFile::Text)) {
+            return;
+        }
+
+        QTextStream out(&file);
+        QString text = ui->textEdit->toPlainText();
+        out << text;
+        file.close();
     }
-
-    currentFile = fileName;
-    parent->setWindowTitle(fileName);
-
-    QTextStream out(&file);
-    QString text = ui->textEdit->toPlainText();
-    out << text;
-    file.close();
 }
 
 void Notes::copy_clicked(Ui::MainWindow *ui){
