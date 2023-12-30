fde83117b1bc733ac8f427d0f9c5564b77e9993f
Spera <stevanpopovic97@gmail.com>
2023-12-30 20:56:43 +0100

    radim na testovima ide gas

--- matfHub/test/matrix.test.cpp
+++ matfHub/test/matrix.test.cpp
@@ -6,11 +6,34 @@
 
 TEST_CASE("Matrix calculator", "[class]"){
 
-    SECTION(""){
-        
+    SECTION("Method getShape returns dimensions of matrix"){
+        Matrix* matrix = new Matrix(3, 5);
 
-    	REQUIRE(true);
-        
+        auto [x, y] = matrix->getShape();
+        bool resultCorrect = (x == 3) && (y == 5);
+
+    	REQUIRE(resultCorrect);
+    }
+
+    SECTION("Method toString returns nicely fomratted string of the matrix"){
+    	Matrix* matrix = new Matrix(2, 2);
+
+    	QString expected = "|\t1\t|\n";
+    	auto output = matrix->toString();
+
+    	REQUIRE(QString::compare(output, expected));
+    }
+
+    SECTION("Methods rows and columns return dimensions of matrix, should be the same as getShape"){
+    	Matrix* matrix = new Matrix(3, 5);
+    	auto [x, y] = matrix->getShape();
+    	
+    	unsigned x1 = matrix->rows();
+    	unsigned y1 = matrix->columns();
+    	bool resultCorrect = (x == x1) && (y = y1);
+
+    	REQUIRE(resultCorrect);
+
     }
    
 }
