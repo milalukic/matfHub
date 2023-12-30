cea530bbc377d377d30de850c1c4282df69162ff
Spera <stevanpopovic97@gmail.com>
2023-12-30 03:35:05 +0100

    podesavanje pocetne skripte za matrice i fmgr

--- matfHub/test/run_test.sh
+++ matfHub/test/run_test.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
 
-echo -ne "choose option:\\n\\tstsatistics: stat \\n\\tparser: parse \\n\\tplotter: plot \\n"
+echo -ne "choose option:\\n\\tstsatistics: st \\n\\tparser: prs \\n\\tplotter: plt \\n\\tmatrix: mx \\n\\tfile manager: fm \\n"
 read OPT
 
-if [[ $OPT != "stat" && $OPT != "parse" && $OPT != "plot" ]]
+if [[ $OPT != "st" && $OPT != "prs" && $OPT != "plt" && $OPT != "mx" && $OPT != "fm" ]]
 then
     echo "Wrong input"
     echo "Exiting..."
 fi
 
 # 
-if [[ $OPT == "stat" ]]
+if [[ $OPT == "st" ]]
 then
     echo "Running statistics class test"
     ./scripts/run_stat_test.sh
-elif [[ $OPT == "parse" ]]
+elif [[ $OPT == "prs" ]]
 then
     echo "Running parser class test"
     ./scripts/run_parser_test.sh
-elif [[ $OPT == "plot" ]]
+elif [[ $OPT == "plt" ]]
 then
     echo "Running plotter class test"
     ./scripts/run_plotter_test.sh
+elif [[ $OPT == "mx" ]]
+then
+    echo "Running matrix class test"
+    ./scripts/run_matrix_test.sh
+elif [[ $OPT == "fm" ]]
+then
+    echo "Running file manager class test"
+    ./scripts/run_fmanager_test.sh
 fi
 
 # test -d Catch2
