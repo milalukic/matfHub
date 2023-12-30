1d1480b62df678cdb721fddcae4eecfb7347bd65
Matija <matija0201@gmail.com>
2023-12-30 11:23:03 +0100

    ubaceno automatsko izvrsavanje prebuild scripte iz cmake-a.

--- prebuild.sh
+++ prebuild.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
-sudo apt-get install -y liblapack-dev
+apt-get install -y liblapack-dev
 
-sudo apt-get install -y libblas-dev
+apt-get install -y libblas-dev
 
-sudo apt-get install -y libboost-dev
+apt-get install -y libboost-dev
 
-sudo apt-get install -y libarmadillo-dev
+apt-get install -y libarmadillo-dev
 
-sudo apt-get install -y gnuplot
+apt-get install -y gnuplot
 
 if [ ! -d "./matfHub/dependencies" ]; then
     mkdir -p ./matfHub/dependencies
 fi
 
-git clone https://github.com/Waqar144/QSourceHighlite.git ./matfHub/dependencies
+DEPENDENCIES_DIR="./matfHub/dependencies"
+
+if [ ! -e "$DEPENDENCIES_DIR/.git" ] && [ -z "$(ls -A $DEPENDENCIES_DIR)" ]; then
+    # Directory is not a Git repository and is not empty, delete everything
+    rm -rf "$DEPENDENCIES_DIR"/*
+    
+    # Perform the clone
+    git clone https://github.com/Waqar144/QSourceHighlite.git "$DEPENDENCIES_DIR"
+else
+    # Directory is either a Git repository or is empty, consider pulling the latest changes
+    (cd "$DEPENDENCIES_DIR" && git pull)
+fi
 
 
